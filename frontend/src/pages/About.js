613222f6899805d9ab06fe8b273b9922f97145e6
JacobIG <103355465+JacobIG@users.noreply.github.com>
2023-01-22 18:06:00 +0100

    Initial commit

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div style={{ marginTop: "150px" }}>
+      <h2>
+        {" "}
+        Aplikacja do zarządzania użytkownikami wykorzystująca Node.js, Express Js i routing.
+      </h2>
+    </div>
+  );
+};
+
+export default About;
